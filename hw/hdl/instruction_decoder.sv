cd1ebb97cd9d94ffe36703437684437910253a85
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2023-10-18 19:05:11 -0700

    create instruction_decoder module and tb

---
+++ hw/hdl/instruction_decoder.sv
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+// instruction_decoder: reads in 32 bit micro-instructions
+// and decodes into various signals for other modules
+
+module instruction_decoder(
+
+    );
+endmodule
---
+++ hw/testbenches/instruction_decoder_tb.sv
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+
+// instruction_decoder_tb: testbench for instruction_decoder
+
+module instruction_decoder_tb(
+
+    );
+endmodule
