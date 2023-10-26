9e6092126619752e0599e44b835e1231d3f3ba60
JaiAdi1 <jayashree.v.adivarahan@gmail.com>
2023-10-25 17:44:01 -0700

    initial versions of instruction_decoder code

--- hw/hdl/instruction_decoder.sv
+++ hw/hdl/instruction_decoder.sv
@@ -3,6 +3,42 @@
 // and decodes into various signals for other modules
 
 module instruction_decoder(
+	input logic [31:0] input_signal,
+	output logic wr_en,
+	output logic rd_en,
+	output logic go,
+	output logic [14:0] wr_addr,
+	output logic [14:0] wr_data,
+	output logic [14:0] rd_start_addr,
+	output logic [14:0] rd_end_addr
+);
+//if the first bit of input_signal is a 1, assign go to be a 1
+assign go = (input_signal[31] == 1'b1) ? 1 : 0;
+//check first two bits of the input signal to see if we should read or write
+assign wr_en = (input_signal[31:30] == 2'b00) ? 1 : 0;
+assign rd_en = (input_signal[31:30] == 2'b01) ? 1 : 0;
 
-    );
+//assigning values to the remaining outputs in the module declaration
+initial begin
+	if (wr_en) begin
+		wr_addr = input_signal[29:15];
+		wr_data = input_signal[14:0];
+		rd_start_addr = 15'b0;
+		rd_end_addr = 15'b0;
+	end
+	else if (rd_en) begin
+		rd_start_addr = input_signal[29:15];
+		rd_end_addr = input_signal[14:0];
+		wr_addr = 15'b0;
+		wr_data = 15'b0;
+	end
+	else begin
+		//initialize all output vars to 0
+		wr_addr = 15'b0;
+		wr_data = 15'b0;
+		rd_start_addr = 15'b0;
+		rd_end_addr = 15'b0;
+	end
+end
+
 endmodule
--- hw/testbenches/instruction_decoder_tb.sv
+++ hw/testbenches/instruction_decoder_tb.sv
@@ -2,7 +2,41 @@
 
 // instruction_decoder_tb: testbench for instruction_decoder
 
-module instruction_decoder_tb(
+module instruction_decoder_tb;
+	logic [31:0] test_input;
+	logic wr_en_test;
+	logic rd_en_test;
+	logic go_test;
+	logic [14:0] wr_addr_test;
+	logic [14:0] wr_data_test;
+	logic [14:0] rd_start_addr_test;
+	logic [14:0] rd_end_addr_test;
 
-    );
+	instruction_decoder decoder (
+		.input_signal(test_input),
+		.wr_en(wr_en_test),
+		.rd_en(rd_en_test),
+		.go(go_test),
+		.wr_addr(wr_addr_test),
+		.wr_data(wr_data_test),
+		.rd_start_addr(rd_start_addr_test),
+		.rd_end_addr(rd_end_addr_test)
+	);
+
+	//clock creation (check if needed?)
+	logic clk = 0;
+	always #5 clk = ~clk;
+
+	initial begin
+		//initializing test_input
+		test_input = 32'b0000_0000_0000_0000_0000_0000_0000_0000;
+
+		//passing in first test_input
+		#10 test_input = 32'b01_001000010000100_010000100011100;
+
+		//display results
+		$display("Test 1: Input = %b, wr_en = %b, rd_en = %b, go = %b, wr_addr = %b, wr_data = %b, rd_start_addr = %b, rd_end_addr = %b", test_input, wr_en_test, rd_en_test, go_test, wr_addr_test, wr_data_test, rd_start_addr_test, rd_end_addr_test);
+		$finish;
+	end
+
 endmodule
