bf521e93cb3d4942e5a1c999b51a134e72fd2908
JaiAdi1 <jayashree.v.adivarahan@gmail.com>
2023-11-23 22:31:11 -0800

    updated param file

---
+++ hw/hdl/instruction_decoder_params.sv
@@ -0,0 +1,6 @@
+package instruction_decoder_params;
+        localparam INPUT_SIGNAL_LENGTH = 32;
+        localparam WRITE_DATA_LENGTH = 16;
+        localparam WRITE_ADDRESS_LENGTH = 14;
+	localparam READ_ADDRESS_LENGTH = 14;
+endpackage
