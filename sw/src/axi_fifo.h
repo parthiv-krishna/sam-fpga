049f0ac725192aef4fc3ebb044b05093abd7837b
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2024-01-10 22:37:08 -0800

    SamFpga abstraction level implemented

--- sw/src/axi_fifo.h
+++ sw/src/axi_fifo.h
@@ -8,21 +8,21 @@
 
 class AxiFifo {
 public:
-	AxiFifo(u16 device_id);
+  AxiFifo(u16 device_id);
 
-	void send(const std::vector<u32>& txData);
-	std::vector<u32> receive();
+  void send(const std::vector<u32>& txData);
+  std::vector<u32> receive();
 
-	static const size_t kBytesPerWord = 4;
-	static const size_t kWordsPerPacket = 4;
-	static const size_t kMaxPackets = 64;
-	static const size_t kMaxWordsInBuffer = kWordsPerPacket*kMaxPackets;
+  static const size_t kBytesPerWord = 4;
+  static const size_t kWordsPerPacket = 4;
+  static const size_t kMaxPackets = 64;
+  static const size_t kMaxWordsInBuffer = kWordsPerPacket*kMaxPackets;
 
 private:
-	void error(const std::string& s);
+  void error(const std::string& s);
 
-	XLlFifo _fifo;
-	XLlFifo_Config* _config;
+  XLlFifo _fifo;
+  XLlFifo_Config* _config;
 
 };
 
