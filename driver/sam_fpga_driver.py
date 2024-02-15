1130db1a40ecf4cc830c584534d57758e4606305
JaiAdi1 <jayashree.v.adivarahan@gmail.com>
2024-02-14 18:09:03 -0800

    edits to sam_fpga_driver to generate sam.store/go/load commands given input tensor files

--- driver/sam_fpga_driver.py
+++ driver/sam_fpga_driver.py
@@ -4,6 +4,7 @@ import logging
 import pathlib
 import random
 import serial
+import json
 from typing import List, Optional, Tuple
 
 logging.basicConfig(level=logging.INFO)
@@ -14,11 +15,11 @@ class DummySerial:
         self.baudrate = 0
 
     def write(self, bytestring: bytes):
-        logging.info(f"DummySerial.write(): Would have sent {bytestring}")
+        logging.info(f"DummySerial.write(): Would have sent 0x{bytestring.hex()}")
 
     def read(self, n: int) -> bytes:
         bytestring = bytes(random.getrandbits(8) for _ in range(n))
-        logging.info(f"DummySerial.read(): returning random data {bytestring}")
+        logging.info(f"DummySerial.read(): returning random data 0x{bytestring.hex()}")
         return bytestring
 
     def close(self):
@@ -47,12 +48,12 @@ class SamFpgaDriver:
     def send_uint32(self, i: int):
         # assert 0 < i < 2^32?
         bytestring = i.to_bytes(4, "big")
-        logging.debug(f"Sending {bytestring}")
+        logging.debug(f"Sending 0x{bytestring.hex()}")
         self.serial_port.write(bytestring)
 
     def read_uint32(self) -> int:
         bytestring = self.serial_port.read(4)
-        logging.debug(f"Received {bytestring}")
+        logging.debug(f"Received 0x{bytestring.hex()}")
         return int.from_bytes(bytestring, "big")
             
     def store(self, addr: int, value: int):
@@ -90,13 +91,69 @@ class SamFpgaDriver:
         logging.debug(f"Go")
         self.send_uint32(inst)
 
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.readlines()
+
 def main(serial_port: pathlib.Path, serial_baud: int):
     sam = SamFpgaDriver(serial_port, serial_baud)
+
+    with open('input_data_tracker.json', 'r') as json_file:
+        data = json.load(json_file)
+
+    # Iterate over each key-value pair in the JSON data
+    for key, value in data.items():
+        #keys are B, C in this case,  value refers to all the info entered under each matrix/tensor name
+        banks_crd_seg = value['banks_crd_seg'] 
+        input_seg = value['input_seg']
+        input_crd = value['input_crd']
+        value_file = value['value']
+        shape_file = value['shape']
+
+        logging.info(f"Writing tensor {key}")
+        logging.info(f"Writing crd, seg for {key}")
+        for i in banks_crd_seg:
+            seg_data = read_file(input_seg[i%5]) #right now mod 5 bcz reading in two tensors, all with modes 0,1,2 - might need to change this later
+            crd_data = read_file(input_crd[i%5])
+
+            bank = i #takes i val (bank number) and converts to 4 bit binary representation
+            addr_needed = len(seg_data)+len(crd_data)
+            combined_list = seg_data + crd_data
+            combined_list = [item.strip() for item in combined_list] #remove '/n' chars
+            for addr in range(addr_needed):
+                spec_addr_to_store = addr
+                full_addr_store = (bank << 10) | spec_addr_to_store
+                sam.store(full_addr_store, int(combined_list[addr]))
+            
+        #storing value and shape
+        logging.info(f"Writing value for {key}")
+        value_data = read_file(value_file)
+        value_data_final = [item.strip() for item in value_data] 
+        value_bank = data[key]["bank_value"]
+        val_spec_addr = 0
+        for value in value_data_final:
+            full_addr_val_store = (value_bank << 10) | val_spec_addr
+            sam.store(full_addr_val_store, int(value_data_final[val_spec_addr]))
+            val_spec_addr = val_spec_addr+1
+
+        logging.info(f"Writing shape for {key}")
+        shape_data = read_file(shape_file)
+        shape_data_final = [item.strip() for item in shape_data] 
+        shape_bank = data[key]["bank_shape"]
+        shape_spec_addr = 0
+        for shape_dim in shape_data_final:
+            full_addr_shape_store = (shape_bank << 10) | shape_spec_addr
+            sam.store(full_addr_shape_store, int(shape_data_final[shape_spec_addr]))
+            shape_spec_addr = shape_spec_addr+1
+
+    sam.go() #wait until complete - potentially can be added to sam.go functionality
+
+    #can read out output tensors using sam.load when an app completes running
 
     # example
     sam.store(0, 42)
     rx = sam.load(0, 1)
-    print(f"loaded {rx}")
+    logging.info(f"loaded {rx}")
     sam.go()
 
 if __name__ == "__main__":
